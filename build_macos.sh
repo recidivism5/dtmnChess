3a7d3ea26beb6ce75fa600cd9645fde0d5fce785
Ian Bryant <ijb2020syr@gmail.com>
2022-12-28 15:34:16 -0500

    renamed main.c to dtmnChess.c

--- build_macos.sh
+++ build_macos.sh
@@ -2,7 +2,7 @@ n = "dtmnChess"
 mkdir $n.app
 mkdir $n.app/Contents
 mkdir $n.app/Contents/MacOS
-clang -framework Cocoa -x objective-c -Os main.c -o $n.app/Contents/MacOS/$n
+clang -framework Cocoa -x objective-c -Os $n.c -o $n.app/Contents/MacOS/$n
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
 <plist version=\"1.0\">
 <dict>
--- build_windows.bat
+++ build_windows.bat
@@ -2,6 +2,6 @@
 if not defined DevEnvDir (
     call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 )
-cl /nologo /O1 /FC /std:c11 /w /Tc main.c kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib dwmapi.lib
-rm main.obj
+cl /nologo /O1 /FC /std:c11 /w /Tc dtmnChess.c kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib dwmapi.lib
+rm dtmnChess.obj
 exit /b 0
