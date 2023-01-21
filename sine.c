a3ae45c804056008f9f3803b1d3869c4f89a91f2
Ian Bryant <ijb2020syr@gmail.com>
2023-01-21 00:39:55 -0500

    I have discovered that a sound must start with 0 for wasapi to play it

--- sine.c
+++ sine.c
@@ -15,7 +15,7 @@ DEFINE_GUID(CLSID_MMDeviceEnumerator,0xBCDE0395,0xE52F,0x467C,0x8E,0x3D,0xC4,0x5
 DEFINE_GUID(IID_IMMDeviceEnumerator,0xA95664D2,0x9614,0x4F35,0xA7,0x46,0xDE,0x8D,0xB6,0x36,0x17,0xE6);
 DEFINE_GUID(IID_IAudioClient,0x1CB9AD4C,0xDBFA,0x4c32,0xB1,0x78,0xC2,0xF5,0x68,0xA7,0x03,0xB2);
 DEFINE_GUID(IID_IAudioRenderClient,0xF294ACFC,0x3146,0x4483,0xA7,0xBF,0xAD,0xDC,0xA7,0xC2,0x60,0xE2);
-i8 sound[]={0,-4,-15,-37,-34,10,42,48,29,5,-13,-23,-16,-1,5,10,5,2,1,-9,-13,-5,5,11,13,9,3,-4,-10,-18,-20,-15,-7,3,10,9,8,11,11,10,3,0,-1,-2,-3,-5,-5,-5,-6,-6,-4,-2,0,2,4,6,5,4,3,2,1,0,0,-2,-4,-5,-6,-5,-1,3,5,4,2,-1,-2,-2,-2,-1,0,1,1,0,0,-1,-1,1,3,4,2,-1,-5,-6,-6,-5,-2,1,4,6,5,3,1,1,0,0,-1,-2,-3,-4,-3,-2,0,2,3,3,2,0,-1,-1,-1,-1,0,-1,-1,-2,-2,-3,-3,-3,-2,-1,0,0,0,1,1,1,0,-1,-1,-2,-2,-1,0,2,2,2,1,-1,-3,-4,-3,-2,-1,0,};
+i8 sound[]={0,2,-10,-37,-94,-88,26,108,124,74,15,-31,-58,-39,-2,13,26,15,5,4,-24,-32,-13,13,29,34,23,10,-9,-25,-45,-49,-38,-18,8,26,23,21,29,28,26,9,0,-1,-5,-7,-11,-12,-12,-14,-14,-10,-5,2,7,11,15,15,12,9,6,3,2,1,-4,-9,-13,-15,-11,-2,7,12,12,6,-1,-5,-5,-4,-1,1,3,3,2,0,-2,-1,3,8,10,6,-3,-11,-15,-15,-12,-5,3,12,16,13,8,4,2,2,1,-1,-4,-7,-9,-8,-5,1,7,9,9,6,2,-1,-2,-1,-1,0,-1,-2,-3,-5,-7,-8,-7,-4,-2,0,1,2,4,4,4,2,-1,-3,-5,-5,-2,2,5,6,6,3,-2,-6,-9,-7,-4,-1,1,2,2,2,1,1,0,0,1,1,0,};
 int main(){
     IMMDeviceEnumerator* pEnumerator = NULL;
     IMMDevice* pDevice = NULL;
@@ -50,11 +50,6 @@ int main(){
     pAudioClient->lpVtbl->GetService(pAudioClient,&IID_IAudioRenderClient,&pRenderClient);
     unsigned int frameCount = sizeof(sound)*pwfx->nSamplesPerSec/8000;
     pRenderClient->lpVtbl->GetBuffer(pRenderClient,frameCount,&pData);
-    /*
-    8000 hz means 1 frame every 1/8000 sec,
-    48000 hz means 1 frame every 1/48000 sec,
-    48000/8000 = 6 frames of 48000hz = 1 frame of 8000 hz
-    */
     if (fmt == WAVE_FORMAT_PCM){
         if (pwfx->wBitsPerSample == 8){
             for (u32 i = 0; i < frameCount; i++){
